8d44c1f733d64c6684ba8525a8d6dba47ccebdaa
fpapado <fotis@futurice.com>
2019-01-20 15:51:38 +0200

    Tweak article blocks

--- src/_includes/components/MarkdownBlock.js
+++ src/_includes/components/MarkdownBlock.js
@@ -1,6 +1,6 @@
 module.exports = function(content, { className = '' } = {}) {
   return `
-    <div class="article f4 nested-copy-line-height nested-headline-line-height nested-copy-separator nested-img measure-prose ${className}">
+    <div class="article nested-img measure-prose ${className}">
         ${content}
     </div>`;
 };
