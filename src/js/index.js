69645c0c4487afde4a94dfcefc1e38a1669d0f1d
fpapado <fotis@futurice.com>
2019-01-21 15:22:16 +0200

    Remove unused polyfills

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,8 @@
-import * as polyfills from './polyfills';
 import * as MakeCpInteractive from './MakeCpInteractive';
 
 /* The main entry point of the application */
 
 const init = () => {
-  polyfills.init();
   MakeCpInteractive.init();
 };
 
