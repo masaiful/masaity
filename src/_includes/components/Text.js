8751bc16de05669f92ebab12c80f16c47065f119
fpapado <fotis@futurice.com>
2019-01-21 15:29:47 +0200

    Make Text consistent with .article

--- src/_includes/components/Text.js
+++ src/_includes/components/Text.js
@@ -1,6 +1,6 @@
 module.exports = function(content, { className = '' } = {}) {
   return `
-    <p class="mv0 f4 lh-copy ${className}">
+    <p class="mv0 f5 f4-m f3-l lh-copy ${className}">
         ${content}
     </p>`;
 };
