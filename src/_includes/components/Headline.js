eced44ddcf2c89f2956868bb93712c52e65a8b2b
Fotis Papadogeorgopoulos <fotis@futurice.com>
2020-03-21 11:45:52 +0200

    Remove unused classes

--- src/_includes/components/Headline.js
+++ src/_includes/components/Headline.js
@@ -3,7 +3,7 @@
 module.exports = function(content, { level = 2, className = '' } = {}) {
   const hx = 'h' + Math.min(level, 6);
   return `
-    <${hx} class="mv5 mv6-ns f1 f-headline-ns fg-primary fw9 lh-title ${className}">
+    <${hx} class="mv5 mv6-ns f1 f-headline-ns fg-primary fw7 lh-title ${className}">
         ${content}
     </${hx}>`;
 };
