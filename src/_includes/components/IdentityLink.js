b48b921f2e069555031dc06eba80f470cc098f89
Fotis Papadogeorgopoulos <fotis@futurice.com>
2020-04-12 11:42:52 +0300

    Add styles back in

--- src/_includes/components/IdentityLink.js
+++ src/_includes/components/IdentityLink.js
@@ -1,11 +1,17 @@
+const { html } = require('common-tags');
+
 /**
  * Like link, but for links that handle identification,
  * such as Github and Twitter.
  * Sets rel="me"
  */
 module.exports = function(content, { href = false, className = '' } = {}) {
-  return `
-    <a href="${href}" class="fg-accent-lighter hover-fg-accent-2 transition-fg focus-tdn enhanced-outline ${className}" rel="me">
-        ${content}
-    </a>`;
+  return html`
+    <a
+      href="${href}"
+      class="fg-accent-lighter hover-fg-accent-2 transition-fg focus-tdn enhanced-outline ${className}"
+      rel="me"
+      >${content}</a
+    >
+  `;
 };
